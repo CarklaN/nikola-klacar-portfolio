91cdc8556fb0be93b1b457fd369a5609e8c8a26d
Nikola Klacar <63237081+CarklaN@users.noreply.github.com>
2026-01-30 01:09:09 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'nikola-klacar-portfolio',
+  base: '/nikola-klacar-portfolio/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
